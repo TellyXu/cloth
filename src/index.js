2f2afc6f7245937de1716bbb4f1edeb35d9002ee
TellyXu <xutelly@gmail.com>
2023-12-10 18:14:38 -0500

    hello world

--- src/index.js
+++ src/index.js
@@ -56,10 +56,10 @@ root.render(
       <Route path="/profile-page" exact element={<ProfilePage />} />
       <Route path="/register-page" exact element={<RegisterPage />} />
       <Route path="/reset-page" exact element={<ResetPage />} />
-      <Route path="/" element={<Navigate to="/presentation" replace />} />
 
       <Route path="/UploadPage" exact element={<UploadPage />} />
       <Route path="/test" exact element={<Test />} />
+      <Route path="/" element={<Navigate to="/Test" replace />} />
 
     </Routes>
   </BrowserRouter>
